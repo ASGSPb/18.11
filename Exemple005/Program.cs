536f8165828126aa6e591ed54b080ba32bb3e5f8
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-19 12:46:07 +0300

    маша

---
+++ Exemple005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mascha")
+{
+    Console.WriteLine("Привет Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
